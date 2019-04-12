f4701d6aeb8ec1c4603e33cff7cb4e04f0668277
Mike Duncan <mduncan2600@gmail.com>
2019-04-11 22:29:23 -0400

    Updates

---
+++ Introduction/ThePowerOfIEnumerable/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePowerOfIEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Mike" },
+                new Employee { Id = 2, Name = "Trav" }
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>
+            {
+                new Employee { Id = 3, Name = "Bill" },
+                new Employee { Id = 4, Name = "Gail" }
+            };
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
